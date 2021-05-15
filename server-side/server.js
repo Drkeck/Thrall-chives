4c5f48557f7357ab3d9dbeb7b6e69dc2197cdeec
Drkeck <k3ckl3y.a@gmail.com>
2021-05-15 00:59:41 -0400

    had to change the file structure to make room for electron, wanted to make this more an application on the desktop then make it one thats done on the web, especially with domain costs for things i don't want to jump into hosting a site that's only really ment for one person.

--- server-side/server.js
+++ server-side/server.js
@@ -1,2 +1,18 @@
-const express = require('express');
 const path = require('path');
+const { app, BrowserWindow} = require('electron');
+
+function createBrowserWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    
+})
