d3d645ded6cb3ff455ff4488c2029e902a33577d
Drkeck <k3ckl3y.a@gmail.com>
2021-05-15 11:35:29 -0400

    added some more peramaters for the window, will be expanding on the look and feel of the application before implimenting the ability to make and manipulate pdfs/worddocs

---
+++ main.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs')
+const { app, BrowserWindow } = require('electron');
+
+//this creates the browser window, and its paramaters. 
+function createBrowserWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+//this will be run when the app is started and calls the window creation function.
+app.whenReady().then(() => {
+    createBrowserWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+          }
+        })
+    })
+      
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+          app.quit()
+    }
+})
