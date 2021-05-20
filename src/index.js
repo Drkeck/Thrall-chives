196966beb5b7b30dc1dafb91707f01fdb564dfdd
Drkeck <k3ckl3y.a@gmail.com>
2021-05-20 14:28:18 -0400

    have been adding to and trying to make the appearance as close to the Automata style, currently the 'buttons' are throwing me off. the minimize and close buttons do have something in the switch statement to know they are working when clicked but proper functionality has yet to be implimented, i need to look into how to do that part.

--- src/index.js
+++ src/index.js
@@ -6,22 +6,27 @@ function eventhandler (event) {
     event.preventDefault()  
     var id = event.target.id;
     let text;
+    let html;
     switch (id) {
         case "post":
-            text = "New Client Info";
+            text = "New Client";
+            html = "<h2 class='title-secondary'>Client Info Form</h2>"
             break;
         case "get":
-            text = "Find Existing Client";
+            text = "Find Client";
+            html = "<h2 class='title-secondary'>Finding Client...</h2>"
             break
         case "exit":
+            console.log('this will exit the application');
             break
         case "mini":
             //add minimization
+            console.log('this will minimize the application');
             break
     }
     //text.
     if (!text) {return}
-    page1.innerHTML = '<h2 class="title-main">' + text + '</h2>';
+    page1.innerHTML = '<h1 class="title-main">' + text + '</h1>' + html;
 
 }
 
