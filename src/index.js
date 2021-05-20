2ec7cfb55dd9d43a03a24981a5a12bfceb486557
Drkeck <k3ckl3y.a@gmail.com>
2021-05-19 23:22:36 -0400

    established some basic javascript to change through the application.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // this will be filled in later
 const menu = document.getElementById('menu');
+const page1 = document.getElementById('main-page');
 
 function eventhandler (event) {
     event.preventDefault()
     var target = event.target.id
     if (target === "menu") {
-        console.log("JOB DONE!")
-    }
+        page1.innerHTML = "<h1 class='title-main'></h1>";
+    }   
 }
 
 addEventListener('click', eventhandler);
