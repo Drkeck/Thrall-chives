2819cd7a4a7dae710a0d2773d87b6c14dfca3474
Drkeck <k3ckl3y.a@gmail.com>
2021-06-06 11:40:19 -0400

    added a html that would make the home page function for the app so i can have a proper return call vs a reload

--- server-side/Routes/Homelist.js
+++ server-side/Routes/Homelist.js
@@ -1,1 +1,20 @@
 // THIS IS COPY PASTE FROM INDEX.HTML BUT FOR THE TIME BEING I AM NOT WORRYING ABOUT IT.
+function homeList () {
+    return`
+    <h1 class="title-main">Data Management:</h1>
+    <figure>
+        <figcaption class="title-secondary">Find Client..</figcaption>
+        <ul class="main-container list-container">
+            <li class="list-item main-li post button" id="post">
+                <h3 class="main-li-text" id="post">New Client</h3>
+            </li>
+            <hr/>
+            <li class="list-item main-li put button" id="get">
+                <h3 class="main-li-text" id="get">Find Client</h3>
+            </li>
+        </ul>
+    </figure>
+    `
+}
+
+module.exports = homeList
